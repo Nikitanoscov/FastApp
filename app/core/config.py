d18f3fbe543efa73e331312300866838ba426cca
Nikita Noskov <nik030205@mail.ru>
2025-03-19 18:26:00 +0300

    Create base app settings.

---
+++ app/core/config.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+
+class RunConfig(BaseModel):
+    host: str = '0.0.0.0'
+    port: int = 8000
+
+
+class APIPrefix(BaseModel):
+    prefix_api: str = '/api/v1'
+
+
+class Settings(BaseSettings):
+    run: RunConfig = RunConfig()
+    api: APIPrefix = APIPrefix()
+
+
+settings = Settings()
